105ec647133414366cfff9e6bd4d1f100576928e
Songkeit <6210742083@student.tu.ac.th>
2022-06-30 23:01:10 +0700

    beginner CSS

--- src/Page/Login.js
+++ src/Page/Login.js
@@ -3,10 +3,10 @@ import Axios from "axios";
 import SelectFile from "./SelectFile";
 import { Navigate } from "react-router-dom";
 import { useNavigate } from 'react-router'
-
-
-
-const Login = ({authenticate}) => {
+import '../Page/Login.css'
+import { MDBInput } from "mdbreact";
+import Header from "./Header";
+const Login = ({ authenticate }) => {
     const [username, setUserneme] = useState("")
     const [password, setPassword] = useState("")
     const [loginState, setLoginState] = useState("")
@@ -14,16 +14,16 @@ const Login = ({authenticate}) => {
     const navigate = useNavigate();
 
 
-    const login = () =>{
-        Axios.post("http://localhost:3001/login",{
-            username:username,
-            password:password,
-        }).then((response) =>{
-            if (response.data.message){
+    const login = () => {
+        Axios.post("http://localhost:3001/login", {
+            username: username,
+            password: password,
+        }).then((response) => {
+            if (response.data.message) {
                 setLoginState(response.data.message)
                 setcheck("false")
                 navigate("/")
-            }else{
+            } else {
                 setcheck(true)
                 setLoginState(response.data[0].UserName)
                 setcheck("true")
@@ -31,33 +31,48 @@ const Login = ({authenticate}) => {
                 navigate("/selectfile")
 
             }
-            
+
         })
     }
     return (
-        <div className="LoginPage">
-            <div className="Login">
-                <h1>login</h1>
-                <input
-                    type="text"
-                    placeholder="username..."
-                    onChange={(e) => {
-                        setUserneme(e.target.value);
-                    }} />
+        <body>
+            <div className="container">
+                <div className="Login">
+                    <div className="header">login</div>
+                    <div className="topic-lable">Username</div>
 
-                <input
-                    type="password"
-                    placeholder="password..."
-                    onChange={(e) => {
-                        setPassword(e.target.value);
-                    }}
-                />
-                <button onClick={login}> button Login</button>
+                    <div class="topic-username">
+                        <input
+                            type="text"
+                            class="form-input"
+                            autoComplete="off"
+                            placeholder=" "
+                            onChange={(e) => {
+                                setUserneme(e.target.value);
+                            }} />
+                            <label for="username" class="form--lable">Username</label>
+                    </div>
+
+                    <div className="topic-lable">Password</div>
+                    <div className="topic-lable">
+                        <input
+                            type="password"
+                            placeholder="password..."
+                            onChange={(e) => {
+                                setPassword(e.target.value);
+                            }}
+                        />
+                    </div>
+                    <div style={{padding:20}}>
+                        <button type="button" class="btn btn-outline-danger"  onClick={login}> button Login</button>
+                    </div>
+
+                </div>
+
             </div>
-                    <h1>{loginState}</h1>
-                    <h1>{check}</h1>
-        </div>
+        </body>
+
     );
-    
+
 }
 export default Login;
